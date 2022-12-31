a057e4c73f57677047c6ca7f707a180e2c6b8ee4
Angelo Malada <angelo.malada@gmail.com>
2022-12-31 16:08:41 +0100

    added project

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,86 +1,80 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import Link from "next/link";
+import React, { MouseEventHandler, useEffect } from "react";
 
-const Home: NextPage = () => {
+type Card = {
+  id: number;
+  name: string;
+  desc: string;
+  image_url: string;
+}[];
+
+type Cards = {
+  cards: Card;
+};
+
+const HomePage = () => {
+  const [data, setData] = React.useState<Cards>();
+
+  const [click, setClick] = React.useState<boolean>(false);
+  useEffect(() => {
+    const fetchData = async () => {
+      // fetch from api
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/getCards`);
+      const json = await res.json();
+
+      setData(json);
+    };
+    fetchData();
+  }, [click]);
+
+  type Click = (name: string) => MouseEventHandler<HTMLButtonElement>;
+
+  const handleClick = async (idFor: number) => {
+    setClick((prev) => !prev);
+    //do post method with fetch
+    //set name against to be the card that does not match the name
+    const idAgainst = data?.cards.filter((card) => card.id !== idFor)[0].id;
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/vote?a=${idAgainst}&b=${idFor}`,
+      {
+        method: "POST",
+      }
+    );
+  };
+
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
+    <div>
+      <Link href={"/results"}>
+        <h1 className="text-4xl text-center  w-fit mx-auto mt-10 p-10 bg-blue-500 rounded-xl">
+          Results
         </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
+      </Link>
+      <div className="flex flex-row">
+        {data?.cards.map((card) => (
+          <div
+            key={card.id}
+            className="flex  w-1/2 flex-col mx-auto items-center  h-screen"
           >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
+            <h1 className="py-10">{card.name}</h1>
+            <img
+              src={card.image_url}
+              alt={card.name}
+              loading="lazy"
+              className=" aspect-auto w-64 pb-20 rounded-lg"
+              // onClick={handleClick(card.name)}
+            />
+            <button
+              onClick={() => handleClick(card.id)}
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            >
+              Vote
+            </button>
+            <p className="px-10">{card.desc}</p>
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default HomePage;
---
+++ pages/results.tsx
@@ -0,0 +1,80 @@
+import Link from "next/link";
+import React, { useEffect } from "react";
+import { useState } from "react";
+
+type Result = {
+  results: {
+    name: string;
+    desc: string;
+    image_url: string;
+    _count: {
+      votesFor: number;
+      votesAgainst: number;
+    };
+  }[];
+};
+const results = () => {
+  const [data, setData] = React.useState<Result>();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      // fetch from jsonplaceholder
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/results`);
+      const json = await res.json();
+
+      setData(json);
+    };
+    fetchData();
+  }, []);
+
+  console.log(data?.results[0]);
+  return (
+    <div>
+      <Link href={"/"}>
+        <h1 className="text-4xl text-center p-10 mt-10 h-[10vh] w-fit mx-auto bg-blue-500 rounded-xl">
+          Home
+        </h1>
+      </Link>
+      {data ? (
+        <div>
+          {data?.results.map((card) => (
+            <div
+              key={card.name}
+              className="flex  w-fit flex-col mx-auto items-center  h-fit"
+            >
+              <h1 className="py-10">{card.name}</h1>
+              <img
+                src={card.image_url}
+                alt={card.name}
+                loading="lazy"
+                className=" aspect-auto w-24 pb-20 rounded-lg"
+              />
+              <div className="flex flex-row gap-4 w-fit">
+                <h1 className="py-2">{card._count.votesFor} Votes for</h1>
+                <h1 className="py-2">
+                  {card._count.votesAgainst} Votes agaisnt
+                </h1>
+                <h1 className="py-2">
+                  {card._count.votesAgainst === 0 && card._count.votesFor !== 0
+                    ? "100%"
+                    : card._count.votesAgainst !== 0 &&
+                      card._count.votesFor === 0
+                    ? "0%"
+                    : (card._count.votesFor /
+                        (card._count.votesFor + card._count.votesAgainst)) *
+                      100}
+                </h1>
+              </div>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <div className="flex justify-center items-center h-[90vh]">
+          <h1>Loading</h1>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default results;
